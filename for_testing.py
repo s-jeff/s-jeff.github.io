31c6217af086b079b164da319a8a9676be321aac
s-jeff <stevejefferson.g@gmail.com>
2023-01-02 19:14:14 +0000

    fixed form with robot integration

--- for_testing.py
+++
@@ -1,9 +0,0 @@
-import requests
-headers = {'User-Agent': 'Mozilla/5.0'}
-gcap = 'googlee'
-payload = {'name':'fromconsole','email':'2meeee', 'subject':'subject','message':'44ee2', 'g-recaptcha-response':'gcap'}
-
-session = requests.Session()
-x= session.post('https://script.google.com/macros/s/AKfycbwCvQNGcg15tOHA7IKrT5ani5E1k_B7cI4fLECHsH8OAzRteLBJVpbUXZAFyxdmyYi2/exec',headers=headers,data=payload)
-
-print(x)
