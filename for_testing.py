f16438f86184b6e3f52ad254641a297c64204627
s-jeff <stevejefferson.g@gmail.com>
2023-01-02 16:48:09 +0000

    debugging form

--- for_testing.py
+++ for_testing.py
@@ -1,7 +1,7 @@
 import requests
 headers = {'User-Agent': 'Mozilla/5.0'}
 gcap = 'googlee'
-payload = {'name':'logtest','email':'meeee', 'subject':'subject','message':'44ee2', 'g-recaptcha-response':'gcap'}
+payload = {'name':'fromconsole','email':'2meeee', 'subject':'subject','message':'44ee2', 'g-recaptcha-response':'gcap'}
 
 session = requests.Session()
 x= session.post('https://script.google.com/macros/s/AKfycbwCvQNGcg15tOHA7IKrT5ani5E1k_B7cI4fLECHsH8OAzRteLBJVpbUXZAFyxdmyYi2/exec',headers=headers,data=payload)
