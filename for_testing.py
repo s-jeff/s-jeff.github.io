ee0dc4ad8cd94a2a7350a9f11b3a0e98e1612ce7
s-jeff <stevejefferson.g@gmail.com>
2023-01-02 16:27:52 +0000

    development commit

--- for_testing.py
+++ for_testing.py
@@ -1,9 +1,9 @@
 import requests
 headers = {'User-Agent': 'Mozilla/5.0'}
-gcap = ''
-payload = {'name':'niceusername','email':'123456', 'subject':'','message':'', 'g-recaptcha-response':'gcap'}
+gcap = 'googlee'
+payload = {'name':'logtest','email':'meeee', 'subject':'','message':'44ee2', 'g-recaptcha-response':'gcap'}
 
 session = requests.Session()
-x= session.post('https://script.google.com/macros/s/AKfycbw0MEur9b_HynJjvL95GDGofBz62ofU7ZlWrVbSBWw/dev',headers=headers,data=payload)
+x= session.post('https://script.google.com/macros/s/AKfycbwCvQNGcg15tOHA7IKrT5ani5E1k_B7cI4fLECHsH8OAzRteLBJVpbUXZAFyxdmyYi2/exec',headers=headers,data=payload)
 
 print(x)
